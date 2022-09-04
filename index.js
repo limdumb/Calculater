d81f7c96edb63ea280c66078d2a6d9104473d269
limdumb <dlaruddls@nate.com>
2022-09-04 16:54:32 +0900

    Feat: 계산함수 추가

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const infoBox = document.querySelector(".calculationBox"); // calculatorBox 하위 엘리멘트
 const result = document.querySelector(".resultnumber")
+const operatorButton = document.querySelector('.operator')
+const calculateResult = document.querySelector('.calculateResult')
+
 let firstNum , operatorInformation , previousKey, previousNum; // 정보를 담는 변수
+
+function calculate(num1, operator, num2) {
+  let result = 0
+  if(operator === undefined) {
+    return infoBox.textContent;
+  } else {
+    result = eval(num1+operator+num2)
+  }
+  return String(result);
+}
 
 infoBox.addEventListener('click', function(event){ //클릭했을때 이벤트 발생
   const target = event.target //HTML 요소정보 가져오기
