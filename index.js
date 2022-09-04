f3c9cfe1e11f5174c4347178fe9552c71f110ea4
limdumb <dlaruddls@nate.com>
2022-09-04 16:30:04 +0900

    Feat: Click Event Number 노출설정

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const infoBox = document.querySelector(".calculationBox"); // calculatorBox 하위 엘리멘트
+const result = document.querySelector(".resultnumber")
 let firstNum , operatorInformation , previousKey, previousNum; // 정보를 담는 변수
 
 infoBox.addEventListener('click', function(event){ //클릭했을때 이벤트 발생
   const target = event.target //HTML 요소정보 가져오기
-  const clickButton = target.textContent;
-  const action = target.classList[0];
+  const clickButton = target.textContent;  // HTML Text 정보
+  const action = target.classList[0];   // 클래스 정보를 가져오기
 
   if(target.matches('button')){
     if (action === 'numbers') {
       console.log(clickButton);
+      if (infoBox.textContent === '0') {
+        result.textContent = clickButton
+      } else {
+        result.textContent += clickButton
+      }
     }
+
   }
 })
